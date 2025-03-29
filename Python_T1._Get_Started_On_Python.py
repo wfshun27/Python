0704364932b5a608eba30f5d1732bbbaa3410604
HS <wfshun@gmail.com>
2025-03-29 15:45:21 +0800

    Upload New Change

---
+++ Python_T1._Get_Started_On_Python.py
@@ -0,0 +1,17 @@
+
+#Python Comment
+# You may try other variations
+print("I am new to programming")
+print("Hello World!")
+
+"""
+THis is a comment
+written in more than just one line
+"""
+print("Helllow World!")
+
+#Activity: Python Script
+x=2
+y=3
+sum = x + y
+print(sum)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Python
-Python Learning
+Learning Python Code
