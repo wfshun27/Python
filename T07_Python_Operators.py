063d1e6c21749b9e085be9f0649fa0af4b9cb978
HS <wfshun@gmail.com>
2025-04-05 08:02:05 +0800

    Update Operator

---
+++ T07_Python_Operators.py
@@ -0,0 +1,15 @@
+#Membership Operators
+a = ['equity','liability','asset','expense','income']
+print('asset' in a)
+print('property' in a)
+
+#Activity: Operator
+a=1;
+b=2;
+c=(a==1) and not (b>3)
+print(c)
+
+
+
+
+
