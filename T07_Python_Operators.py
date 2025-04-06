c8f442906927b9386e0feb01b51f0125992960a8
HS <wfshun@gmail.com>
2025-04-06 09:47:39 +0800

    Changes

--- T07_Python_Operators.py
+++ T07_Python_Operators.py
@@ -1,9 +1,46 @@
-#Membership Operators
+"""
+Operators
+Addition        +
+Subtraction     -
+Multiplication  *
+Division        /
+Exponent        **
+Floor Division  //
+"""
+
+"""
+Compound Operators
+Addition (x=x+1)    +=
+Substraction        -=
+Multiplication      *=
+Division            /=
+Exponent            **=
+Modulus             %=
+Floor Divisions     //=
+"""
+
+"""
+Comparison Operators
+Less Than               <
+Less Than or Equal      <=
+Bigger Than             >
+Greater than or equal   >=
+Equal                   ==
+Not Equal               !=
+"""
+
+# in or not in
+# Membership Operators
 a = ['equity','liability','asset','expense','income']
 print('asset' in a)
 print('property' in a)
 
+# Logical Operators (Boolean Operators)
+# True or False
+# True and False
+
 #Activity: Operator
+#The answer is either True or False
 a=1;
 b=2;
 c=(a==1) and not (b>3)
