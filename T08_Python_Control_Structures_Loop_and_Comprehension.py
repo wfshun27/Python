c82c46b588cc9cfe8ff1618b99898cf8a3d30f10
wfshun27 <wfshun@hotmail.com>
2025-04-11 00:32:58 +0800

    update

--- T08_Python_Control_Structures_Loop_and_Comprehension.py
+++ T08_Python_Control_Structures_Loop_and_Comprehension.py
@@ -13,20 +13,81 @@ elif (stock_price_ABC > 300):
 
 #if=Elif
 #If you change the value of the variable
-stock_price_ABC = 301
+stock_price_ABC = 200
 if (stock_price_ABC > 250):
     print("We will sell the stock and book the profit\n")
 else:
     print("We will sell keep the stock\n")
 
+"""
+Activity: Conditional
+"""
+income = input("What is your income:")
+income = int(income)
+if (income > 8000):
+    print("High Income")
+elif (income > 4000 and income < 8000):
+    print("Medium High Income\n")
+elif (income > 2000 and income < 4000):
+    print("Medium Income\n")
+elif (income > 1000 and income < 2000):
+    print("Medium Low Income\n")
+else:
+    print("Low Income\n")
+
+"""
+Ternary Operator
+"""
+order=200
+discount=25
+if (order >100):
+    print(order*discount/100,"\n"
+          )
+else:
+    print("0\n")
+
+"""
+While Loop
+The while constuct consists of a condition and block of code.
+The general syntax for a 'while' loop is as follow:-
+
+while condition/expression:
+    block of statements
+
+To begin, the condition is evaluted.
+If the condition is true, the 'block  of statements 
+Every time, the condition is checked before executing the block of statements
+This keeps on reporting until the condition becomes false, it comes out of the loop to exexute the other statements.
+"""
 #While Loop Example
 
 a = 0 #Varible
 while a <= 10:
-    a = a + 1.2
+    a = a + 1
     print(a)
 
-print("We are now out of the loop")
+print("We are now out of the loop\n")
+
+"""
+Fibonacci and Golden Ratio in Nature
+Fibonacci and Golden Ratio in Art and Architure
+Golden Ration in Technical Analysis
+"""
+"""
+Activity: Compute Fibonacci Seq and Golden Ratio Using While Loop
+Fibonacci nymber is the sum of the previous 2 fibonacci numbers.
+1,1,2,3,5,8,21,34,...
+
+Use the while loop to 
+* compute the fibonacci sequence up to 144 and 
+* compute the golden ratio which is the ration of two subsequent Fibonacci numbers
+"""
+a,b = 0, 1
+fib=[]
+
+while a <= 144:
+    a = a+1
+    print(a)
 
 #Multiple Assignments
 a,b=0,1
